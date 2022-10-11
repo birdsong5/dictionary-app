e33c9044b2100f625fbdbe5151ea803f31c586fb
birdsong5 <108277183+birdsong5@users.noreply.github.com>
2022-10-11 20:35:57 +0200

    Created HTML structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
+import React from "react";
+
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <h1>Dictionary</h1>
+      </header>
+      <div className="container">
+        <div className="search-section">
+          <h3>Search for a word you want to look up</h3>
+          <form>
+            <input type="search" />
+            <input type="submit" />
+          </form>
+        </div>
+        <div className="photos-section"></div>
+        <div className="definition-section"></div>
+      </div>
     </div>
   );
 }
